0d006ed289aae8fc135e508e97e8b148af42b823
MattiaSeu <50059272+MattiaSeu@users.noreply.github.com>
2022-10-02 02:58:54 +0200

    Modified scripts to work locally

--- scripts/imgtonpz.py
+++ scripts/imgtonpz.py
@@ -2,23 +2,7 @@ from PIL import Image
 import numpy as np
 import os, sys
 
-path = "C:/Users/DaPC/PycharmProjects/ldm1/data/ref_datasets/COCO/val2017"
-dirs = os.listdir(path)
-
-#
-# def resize():
-#     for item in dirs:
-#         if os.path.isfile(path + item):
-#             im = Image.open(path + item)
-#             f, e = os.path.splitext(path + item)
-#             imResize = im.resize((256, 256), Image.ANTIALIAS)
-#             imResize.save(f + '.jpg', 'jpg', quality=80)
-#
-#
-# resize()
-
-
-path_to_files = "C:/Users/DaPC/Desktop/University/NeuralNetworks/datasets/ffhq/resized/"
+path_to_files = "log_dir/models/ldm/cin256" # change with path to image folder to be compressed to npz
 array_of_images = []
 
 for _, file in enumerate(os.listdir(path_to_files)):
@@ -26,5 +10,5 @@ for _, file in enumerate(os.listdir(path_to_files)):
     single_array = np.array(single_im)
     array_of_images.append(single_array)
 
-np.savez(path_to_files + "all_images.npz", array_of_images)  # save all in one file
+np.savez(path_to_files + "all_images.npz", array_of_images)  # save all in one npz archive
 
